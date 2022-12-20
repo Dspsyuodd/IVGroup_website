a90b4e99f9b48474232a9e0ce9dc04ca83f6e417
Dspsyuodd <78037644+Dspsyuodd@users.noreply.github.com>
2022-12-20 16:09:13 +0400

    scroll

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,182 +11,83 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var scrollController = ScrollController();
     return Scaffold(
-      body: WebSmoothScroll(
-        controller: scrollController,
-        scrollOffset: 30,
-        animationDuration: 150,
-        child: SingleChildScrollView(
-          physics: const NeverScrollableScrollPhysics(),
-          controller: scrollController,
-          child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(255, 21, 41, 45),
-                  Color.fromARGB(255, 22, 11, 31),
-                ],
-              ),
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 21, 41, 45),
+                Color.fromARGB(255, 22, 11, 31),
+              ],
             ),
-            child: Column(
-              children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 30, 31, 32),
-                  ),
-                  child: SizedBox(
-                    height: 70,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Spacer(),
-                        Image.asset('assets/images/Logo.png'),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: getText('IV-Group', 20),
-                        ),
-                        const Spacer(
-                          flex: 5,
-                        ),
-                        getText('О компании', 20),
-                        const Spacer(),
-                        getText('Контакты', 20),
-                        const Spacer(),
-                        getText('Наши продукты', 20),
-                        const Spacer(
-                          flex: 6,
-                        ),
-                      ],
-                    ),
+          ),
+          child: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 30, 31, 32),
+                ),
+                child: SizedBox(
+                  height: 70,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Spacer(),
+                      Image.asset('assets/images/Logo.png'),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: getText('IV-Group', 20),
+                      ),
+                      const Spacer(
+                        flex: 5,
+                      ),
+                      getText('О компании', 20),
+                      const Spacer(),
+                      getText('Контакты', 20),
+                      const Spacer(),
+                      getText('Наши продукты', 20),
+                      const Spacer(
+                        flex: 6,
+                      ),
+                    ],
                   ),
                 ),
-                Stack(
-                  children: [
-                    Positioned(
-                      right: -50,
-                      top: 0,
-                      child: SizedBox(
-                        width: 900,
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    right: -50,
+                    top: 0,
+                    child: SizedBox(
+                      width: 900,
+                      height: 1100,
+                      child: Image.asset(
+                        'assets/images/dots2.png',
+                        width: 800,
                         height: 1100,
-                        child: Image.asset(
-                          'assets/images/dots2.png',
-                          width: 800,
-                          height: 1100,
-                          opacity: const AlwaysStoppedAnimation(1),
-                        ),
+                        opacity: const AlwaysStoppedAnimation(1),
                       ),
                     ),
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 342,
-                            bottom: 839,
-                          ),
-                          child: Row(
-                            children: [
-                              const Spacer(),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  getText('IV-Group', 76),
-                                  getText(
-                                      'Современные проблемы - новые решения',
-                                      25),
-                                ],
-                              ),
-                              const Spacer(
-                                flex: 6,
-                              ),
-                            ],
-                          ),
+                  ),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          top: 342,
+                          bottom: 839,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 226),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width / 2.8,
-                                child: FittedBox(
-                                  child: SizedBox(
-                                    width: 668,
-                                    height: 445,
-                                    child: Image.asset(
-                                        'assets/images/About_image.png'),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width / 2.2,
-                                child: FittedBox(
-                                  child: getText(
-                                    'О нас:\n«IV-Group»-современная IT-компания, занимающаяся разработкой\nпрограммного обеспечения. Современные решения- наше все.\nИмеем дела с разработкой приложений с большим количеством\nразличных технологий, что доказывает нашу надежность.\nЗанимаемся продуктовой и заказной разработкой.',
-                                    25,
-                                    spacing: 3,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 676),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: const [
-                              Benefit(
-                                text: 'Мобильная разработка',
-                                imagePath: 'assets/images/Mobile.png',
-                              ),
-                              Benefit(
-                                text: 'Веб разработка',
-                                imagePath: 'assets/images/Web.png',
-                              ),
-                              Benefit(
-                                text: 'Инновационные технологии',
-                                imagePath: 'assets/images/Innovation.png',
-                              )
-                            ],
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 228),
-                          child: Divider(
-                            color: Color.fromARGB(255, 178, 178, 178),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 58),
-                          child: getText('Есть вопросы, обратитесь к нам', 25),
-                        ),
-                        Row(
+                        child: Row(
                           children: [
                             const Spacer(),
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                getText('Контактные данные', 25,
-                                    color: const Color.fromARGB(
-                                        204, 255, 255, 255)),
-                                const Padding(
-                                  padding:
-                                      EdgeInsets.only(bottom: 411, top: 10),
-                                  child: SelectableText(
-                                    'Ivgroup@mail.ru',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontFamily: 'Sansation',
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w400,
-                                      letterSpacing: -0.13,
-                                      fontSize: 25,
-                                    ),
-                                  ),
-                                ),
+                                getText('IV-Group', 76),
+                                getText(
+                                    'Современные проблемы - новые решения', 25),
                               ],
                             ),
                             const Spacer(
@@ -194,52 +95,141 @@ class HomePage extends StatelessWidget {
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                    const Screen(
-                      degrees: 0,
-                      left: 136,
-                      top: 149,
-                    ),
-                    const Screen(
-                      degrees: 100,
-                      right: 138,
-                      top: 198,
-                    ),
-                    const Screen(
-                      degrees: -30,
-                      left: 316,
-                      top: 717,
-                    ),
-                    const Screen(
-                      degrees: 40,
-                      right: 575,
-                      top: 476,
-                    ),
-                    const Screen(
-                      degrees: 60,
-                      right: 123,
-                      top: 1760,
-                    ),
-                    const Screen(
-                      degrees: 0,
-                      left: 216,
-                      top: 1860,
-                    ),
-                    const Screen(
-                      degrees: -30,
-                      left: 340,
-                      bottom: 1036,
-                    ),
-                    const Screen(
-                      degrees: 90,
-                      right: 370,
-                      bottom: 962,
-                    ),
-                  ],
-                )
-              ],
-            ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 226),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 2.8,
+                              child: FittedBox(
+                                child: SizedBox(
+                                  width: 668,
+                                  height: 445,
+                                  child: Image.asset(
+                                      'assets/images/About_image.png'),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width / 2.2,
+                              child: FittedBox(
+                                child: getText(
+                                  'О нас:\n«IV-Group»-современная IT-компания, занимающаяся разработкой\nпрограммного обеспечения. Современные решения- наше все.\nИмеем дела с разработкой приложений с большим количеством\nразличных технологий, что доказывает нашу надежность.\nЗанимаемся продуктовой и заказной разработкой.',
+                                  25,
+                                  spacing: 3,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 676),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: const [
+                            Benefit(
+                              text: 'Мобильная разработка',
+                              imagePath: 'assets/images/Mobile.png',
+                            ),
+                            Benefit(
+                              text: 'Веб разработка',
+                              imagePath: 'assets/images/Web.png',
+                            ),
+                            Benefit(
+                              text: 'Инновационные технологии',
+                              imagePath: 'assets/images/Innovation.png',
+                            )
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 228),
+                        child: Divider(
+                          color: Color.fromARGB(255, 178, 178, 178),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 58),
+                        child: getText('Есть вопросы, обратитесь к нам', 25),
+                      ),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              getText('Контактные данные', 25,
+                                  color:
+                                      const Color.fromARGB(204, 255, 255, 255)),
+                              const Padding(
+                                padding: EdgeInsets.only(bottom: 411, top: 10),
+                                child: SelectableText(
+                                  'Ivgroup@mail.ru',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontFamily: 'Sansation',
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                    letterSpacing: -0.13,
+                                    fontSize: 25,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Spacer(
+                            flex: 6,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Screen(
+                    degrees: 0,
+                    left: 136,
+                    top: 149,
+                  ),
+                  const Screen(
+                    degrees: 100,
+                    right: 138,
+                    top: 198,
+                  ),
+                  const Screen(
+                    degrees: -30,
+                    left: 316,
+                    top: 717,
+                  ),
+                  const Screen(
+                    degrees: 40,
+                    right: 575,
+                    top: 476,
+                  ),
+                  const Screen(
+                    degrees: 60,
+                    right: 123,
+                    top: 1760,
+                  ),
+                  const Screen(
+                    degrees: 0,
+                    left: 216,
+                    top: 1860,
+                  ),
+                  const Screen(
+                    degrees: -30,
+                    left: 340,
+                    bottom: 1036,
+                  ),
+                  const Screen(
+                    degrees: 90,
+                    right: 370,
+                    bottom: 962,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
